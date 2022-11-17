130a74cf516f10e0ef52bc916351fcfc3a1d43b1
David L. Campbell <dcampbell24@gmail.com>
2022-11-16 22:10:39 -0500

    Add log.

--- cubes-rs/src/solve/bin/main.rs
+++ cubes-rs/src/solve/bin/main.rs
@@ -4,6 +4,7 @@ use cubes_rs::{choose_puzzle, solve, write_obj_file, write_obj_file_solution};
 
 fn main() -> Result<(), cubes_rs::Error> {
     let now = Instant::now();
+    env_logger::init();
 
     let (puzzle, puzzle_string) = choose_puzzle()?;
     write_obj_file(&puzzle, &puzzle_string)?;
@@ -15,7 +16,7 @@ fn main() -> Result<(), cubes_rs::Error> {
     write_obj_file_solution(&solutions[0], &puzzle_string)?;
 
     let elapsed_time = now.elapsed();
-    println!(
+    log::info!(
         "Running the program took {} seconds.",
         elapsed_time.as_micros() as f64 / 1_000_000.0
     );
