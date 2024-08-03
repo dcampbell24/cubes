1b4ad99af0abc67de64b70decc5a352b2922894a
David Campbell <dcampbell24@gmail.com>
2024-08-02 22:02:37 -0400

    Add categories, lint clippy::cargo.

--- cubes-rs/src/lib.rs
+++ cubes-rs/src/lib.rs
@@ -1,3 +1,18 @@
+#![deny(
+    clippy::all,
+//  clippy::restriction,
+//  clippy::pedantic,
+//  clippy::nursery,
+    clippy::cargo,
+)]
+#![allow(
+    clippy::module_name_repetitions,
+    clippy::similar_names,
+    clippy::struct_field_names,
+    clippy::too_many_lines,
+    clippy::multiple_crate_versions
+)]
+
 use anyhow::Context;
 use directories::ProjectDirs;
 use serde::{Deserialize, Serialize};
